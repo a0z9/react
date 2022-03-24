bbee8983eae194c3fc9c898dbeb302ed119e1040
Andrei <a0z9@rambler.ru>
2022-03-24 11:05:25 +0300

    es6 modules, v.2

--- app4/index.js
+++ app4/index.js
@@ -1,5 +1,13 @@
 import {Greets1, Greets2} from './Greets.js'
+import * as GreetsModule from './Greets.js'
+import {Greets as G} from "./Greets.js";
 
 var log = console.log
 
 log(Greets1);
+log(GreetsModule.Greets2);
+
+const hello = new G("Салют","Андрей");
+log(hello.sayGreets())
+
+
